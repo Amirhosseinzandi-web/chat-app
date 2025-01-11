4987be73925d181bbe9a2059230b9f316f91f241
Amirhossein Zandi <blackviper933@gmail.com>
2025-01-11 18:30:51 +0330

    add route to get all users in authRoute

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import { login, signUp, logout, updateProfile } from "../controller/authController/authController.js"
+import { login, signUp, logout, updateProfile, getAllUsers } from "../controller/authController/authController.js"
 import { protectRoute } from '../middlewares/protectRoute.js';
 
 
 const router = express.Router();
 
 
-
+router.get("/get-users", getAllUsers)
 router.post("/signup", signUp)
 router.post("/login", login)
 router.post("/logout", logout)
